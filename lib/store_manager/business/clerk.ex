6db4387eebb4a4c51d72fb113c8e777b46e71ec2
brown-lyssie <abrown@nevada.unr.edu>
2023-04-12 18:04:37 -0700

    finished clerk

--- lib/store_manager/business/clerk.ex
+++ lib/store_manager/business/clerk.ex
@@ -2,9 +2,12 @@ defmodule StoreManager.Business.Clerk do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias StoreManager.Business.Store
+
   schema "clerks" do
     field :last_name, :string
-    field :store_id, :id
+    # field :store_id, :id
+    belongs_to :store, Store
 
     timestamps()
   end
